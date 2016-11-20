0a51a019b1bcf9b5d1b59f2a44986225cd3fdfac
Michael Arnott <arnottmj@users.noreply.github.com>
2016-11-20 16:55:54 +0000

    made executable

--- lib/app.rb
+++ lib/app.rb
@@ -1,4 +1,14 @@
+#!/usr/bin/env ruby
 require 'json'
 
-file = File.read('file-name-to-be-read.json')
+requested_time = ARGV[0]
+file = File.read('../Support/availability_slots.json')
 data_hash = JSON.parse(file)
+
+def determine_booking(time_requested, available_slots) 
+
+end
+
+
+
+
